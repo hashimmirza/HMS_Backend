55ab436aa6f4cff5a75a406ed2ee7e8f22d26207
Hashim <Hashim.mirza017@gmail.com>
2021-03-20 18:13:20 +0500

    Hospital routing/controller setup

--- mobile_apis/Routes/index.js
+++ mobile_apis/Routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authenticationRoutes = require('./authentication');
+const hospitalRoutes = require('./hospital');
 
 const routes = express.Router();
 
 routes.use('/auth', authenticationRoutes);
+routes.use('/hospital', hospitalRoutes);
 
 module.exports = routes;
--- mobile_apis/Services/Cron_job.js
+++ mobile_apis/Services/Cron_job.js
@@ -1,6 +1,6 @@
 const cron = require("node-cron");
 
-module.export =  cron.schedule("*/1 * * * *", function() {
+module.export =  cron.schedule("* */1 * * *", function() {
     let nowTime = new Date();
     console.log("Job Updated at" + nowTime);
 
